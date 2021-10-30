2080acafb5d4b327f338dc8bd00a8edde2d6457b
sunny <sunny2004.kim@gmail.com>
2021-10-30 00:25:13 -0500

    make it better

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import cross_origin
 import json
 
 app = Flask(__name__)
@@ -13,29 +14,40 @@ def dump_json(injson):
 
 
 @app.route('/clear', methods=['POST'])
+@cross_origin()
 def clear():
     if request.method == 'POST':
         f=request.json
         if f['key'] == ";F05lUCw%[hUeD?84~XMK{E@OO}4uPdYB4do'?8bH-BlSTuh9#|W=TzS(Pq9e":
             dump_json({'recipes': []})
-            return 'done'
+            out = jsonify(msg='done')
+            return out
         else:
-            return 'invalid key'
+            out = jsonify(msg='invalid key')
+            return out
 
 
 @app.route('/upload', methods=['POST'])
+@cross_origin()
 def add_recipe():
+    print('tfasdf')
     if request.method == 'POST':
-        # f = request.files['file']
-        # f.save(secure_filename(f.filename))
-        f= request.json
-        print(f['name'])
+        f = request.json
 
         todump = import_json()
         todump['recipes'].append(f)
         dump_json(todump)
 
-        return todump
+
+        return jsonify(todump)
+
+
+@app.route('/get', methods=['GET'])
+def return_recipes():
+    jout = import_json()
+    parsed = jsonify(jout)
+    parsed.headers.add("Access-Control-Allow-Origin", "*")
+    return parsed
 
 
 if __name__ == '__main__':
